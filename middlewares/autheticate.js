341d738a9405d917c2a529b41203472ea81f5954
Slutishboy <sohomsaha361@gmail.com>
2024-05-11 22:48:39 +0530

    uspdate

--- middlewares/autheticate.js
+++ middlewares/autheticate.js
@@ -2,18 +2,18 @@ const jwt = require("jsonwebtoken");
 
 const authenticate = async (req, res, next) => {
   // console.log(req);
-  // var cookie = req.cookies.UserToken;
+  var cookie = req.cookies.UserToken;
   //  console.log(cookie);
-  // console.log(req.myHeader);
-  // console.log(cookie);
-  // if (!cookie || !req.myHeader) {
-  if ( !req.myHeader) {
+  const cok=req.headers['authorization']
+  console.log(cookie);
+  if (!cookie || !cok) {
+  // if ( !req.myHeader) {
     res.send({
       status: "User not Authenticated",
     });
     return;
   } else {
-    cookie=req.myHeader;
+    cookie=cok;
     try {
       const ex = jwt.verify(cookie,process.env.privatekey);
       console.log(ex);
